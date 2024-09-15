03c8d790e3e5a0b337717c4b7a0fa96df149a29d
ofaronbi <faronbi_opeyemi@yahoo.com>
2024-09-15 12:10:04 -0400

    Added API test

--- cypress/e2e/DNGTestRegisterNewUser.cy.js
+++ cypress/e2e/DNGTestRegisterNewUser.cy.js
@@ -17,6 +17,73 @@ describe('DineNGo Registration Screen API Test', () => {
         cy.get("#mat-input-6", { timeout: 10000 }).should('be.visible').type("Ilovecookies!");  //input password
         cy.get("button.btn-info").click();   //click 'REGISTER' button 
         cy.get("button.btn-danger").click();  //click 'CANCEL' button
-    })
+    });
+
+    it("Load registration page successfully", () => {
+        cy.request({
+          method: "GET",
+          url: "https://grubgrab.net/register",
+          headers: {
+            Authorization:
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTUyZmYyZDU1MjQ5ZDViNmQyNGM4NTQiLCJhY2NvdW50VHlwZSI6IkNsb3ZlciIsIm9yZ1V1aWQiOiI2NTUyZmYyZDU1MjQ5ZDViNmQyNGM4NTQiLCJpYXQiOjE3MjY0MDg3MTIsImV4cCI6MTc1NzE2NzExMn0.uIO_h3_kaLVznlbE6HnXVHyZ10FxW2o6nmYnIgNSZIw",
+          },
+          failOnStatusCode: false,
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+        });
+      });
+    
+      // API call to test registration page *** Change email and uniqueOrgIdent
+      it("Register a new user successfully", () => {
+        cy.request({
+          method: "POST",
+          url: "https://grubgrab.net/api/users/accountRegistration",
+          headers: {
+            Authorization:
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTUyZmYyZDU1MjQ5ZDViNmQyNGM4NTQiLCJhY2NvdW50VHlwZSI6IkNsb3ZlciIsIm9yZ1V1aWQiOiI2NTUyZmYyZDU1MjQ5ZDViNmQyNGM4NTQiLCJpYXQiOjE3MjY0MDg3MTIsImV4cCI6MTc1NzE2NzExMn0.uIO_h3_kaLVznlbE6HnXVHyZ10FxW2o6nmYnIgNSZIw",
+          },
+          body: {
+            firstName: "Hope",
+            lastName: "John",
+            organization: "Kennesaw State University",
+            uniqueOrgIdent: "@iip.com",
+            platform: "Clover",
+            phoneNumber: 3293567289,
+            username: "johnhope1@gmail.com",
+            email: "johnhope1@gmail.com",
+            password: "Password1",
+            imageData: ""
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200);
+        });
+      });
+    
+      // API call to test if user already exist ** Use data from Register a new user successfully. 
+      it("User already exist", () => {
+        cy.request({
+          method: "POST",
+          url: "https://grubgrab.net/api/users/accountRegistration",
+          headers: {
+            Authorization:
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTUyZmYyZDU1MjQ5ZDViNmQyNGM4NTQiLCJhY2NvdW50VHlwZSI6IkNsb3ZlciIsIm9yZ1V1aWQiOiI2NTUyZmYyZDU1MjQ5ZDViNmQyNGM4NTQiLCJpYXQiOjE3MjY0MDg3MTIsImV4cCI6MTc1NzE2NzExMn0.uIO_h3_kaLVznlbE6HnXVHyZ10FxW2o6nmYnIgNSZIw",
+          },
+          body: {
+            firstName: "Hope",
+            lastName: "John",
+            organization: "Kennesaw State University",
+            uniqueOrgIdent: "@iip.com",
+            platform: "Clover",
+            phoneNumber: 3293567289,
+            username: "johnhope1@gmail.com",
+            email: "johnhope1@gmail.com",
+            password: "Password1",
+            imageData: ""
+          },
+          failOnStatusCode: false,
+        }).then((response) => {
+          expect(response.status).to.eq(400);
+        });
+      });
   });
   
